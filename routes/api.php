952c7a39d755ffb80596c215dbc26fd5ccf90b90
Anang Adi <anang.adi182@gmail.com>
2024-08-16 09:42:00 +0700

    teams endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,36 @@
 <?php
 
 use App\Http\Controllers\Api\CompanyController;
+use App\Http\Controllers\Api\TeamController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/v1')->group(function () {
-    Route::post('/login', [UserController::class, 'login']);
-    Route::post('/register', [UserController::class, 'register']);
+
+    // Auth
+    Route::name('auth.')->group(function () {
+        Route::post('/login', [UserController::class, 'login']);
+        Route::post('/register', [UserController::class, 'register']);
+
+        Route::middleware('auth:sanctum')->group(function () {
+            Route::post('/logout', [UserController::class, 'logout']);
+            Route::get('/me', [UserController::class, 'me']);
+        });
+    });
 
     Route::middleware('auth:sanctum')->group(function () {
-        Route::post('/logout', [UserController::class, 'logout']);
-        Route::get('/me', [UserController::class, 'me']);
+        // Company
+        Route::prefix('companies')->name('companies.')->group(function () {
+            Route::get('', [CompanyController::class, 'index']);
+            Route::post('/update', [CompanyController::class, 'update']);
+        });
 
-        Route::get('/companies', [CompanyController::class, 'index']);
-        Route::post('/companies/update', [CompanyController::class, 'update']);
+        // Team
+        Route::prefix('teams')->name('teams.')->group(function () {
+            Route::get('', [TeamController::class, 'fetch'])->name('fetch');
+            Route::post('', [TeamController::class, 'create'])->name('create');
+            Route::post('update/{id}', [TeamController::class, 'update'])->name('update');
+            Route::delete('{id}', [TeamController::class, 'destroy'])->name('delete');
+        });
     });
 });
